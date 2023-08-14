a18b80a3abb7abf858f2c60fb40c9d6926bd76b4
yahya chami <123872563+dzap00@users.noreply.github.com>
2023-08-14 18:52:15 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "ASCII.h"
+
+
+int main() {
+    begin* Test = new begin;
+    Test->run();
+    solution *Tes = new solution;
+    Tes->ImageToASCII();
+    delete Test;
+    return 0;
+}
